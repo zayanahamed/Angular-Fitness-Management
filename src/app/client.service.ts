81d771a6bb19d0ba0335f3aa2e6b008bdebc0fe0
Zayan <ahamedzayan99@gmail.com>
2024-11-16 15:36:15 +0530

    store data in local storage

--- src/app/client.service.ts
+++ src/app/client.service.ts
@@ -2,35 +2,55 @@ import { Injectable } from '@angular/core';
 import { Client } from './client.model';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class ClientService {
+  private localStorageKey = 'clients';
+
+  constructor() {
+    this.loadClients();
+  }
+
   private clients: Client[] = [];
 
-  getClients(): Client[] {
-    return [...this.clients];
+  private loadClients(): void {
+    const clientsJson = localStorage.getItem(this.localStorageKey);
+    if (clientsJson) {
+      this.clients = JSON.parse(clientsJson);
+    }
+  }
+
+  private saveClients(): void {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(this.clients));
   }
 
   addClient(client: Client): void {
     this.clients.push(client);
+    this.saveClients();
   }
 
   updateClient(updatedClient: Client): void {
-    const index = this.clients.findIndex((c) => c.clientID === updatedClient.clientID);
+    const index = this.clients.findIndex(client => client.clientID === updatedClient.clientID);
     if (index !== -1) {
       this.clients[index] = updatedClient;
+      this.saveClients();
     }
   }
 
   deleteClient(clientID: string): void {
-    this.clients = this.clients.filter((client) => client.clientID !== clientID);
+    this.clients = this.clients.filter(client => client.clientID !== clientID);
+    this.saveClients();
   }
 
   searchClient(clientID: string): Client | undefined {
-    return this.clients.find((client) => client.clientID === clientID);
+    return this.clients.find(client => client.clientID === clientID);
+  }
+
+  getClients(): Client[] {
+    return this.clients;
   }
 
   getVipClients(): Client[] {
-    return this.clients.filter((client) => client.isVIP);
+    return this.clients.filter(client => client.isVIP);
   }
 }
